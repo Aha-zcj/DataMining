e98fdd166da015896fc9aee93397c11d3c1cf506
Aha <tzl77258511@gmail.com>
2012-11-14 14:30:32 +0800

    Initial Commit

---
+++ DataMining/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataMining
+//
+//  Created by Aha on 12-11-14.
+//  Copyright (c) 2012年 Aha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
