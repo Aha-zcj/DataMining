20877396c6ae6e1637858915ca28670296e605df
Aha <tzl77258511@gmail.com>
2012-11-18 23:49:29 +0800

    the first completion

---
+++ DataMining/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  DataMining
+//
+//  Created by Aha on 12-11-14.
+//  Copyright (c) 2012年 Aha. All rights reserved.
+//
+
+#ifndef DataMining_Header_h
+#define DataMining_Header_h
+
+
+
+#endif
